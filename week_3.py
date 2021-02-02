69d9e994e966a53640fb14fa787743907b4a918c
nathalyusl <nathalyusl@gmail.com>
2021-02-01 23:12:53 -0500

    for

--- week_3.py
+++ week_3.py
@@ -1,12 +1,12 @@
 print("hello world")
 
-name = ["Diana", "Moe", "Tarzan"]
+name_list = ["Diana", "Moe", "Tarzan"]
 
-last_name = ["Altamirano", "Win", "Selva"]
+last_name_list = ["Altamirano", "Win", "Selva"]
 
 full_name = []
 
-for i in name:
+for i in range(3):
     name2 = name
     last_name2 = last_name
     full_name = name2 + "" + last_name2
