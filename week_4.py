e81713fae5544ff7509211939c24f50a01496e33
nathalyusl <nathalyusl@gmail.com>
2021-02-02 16:23:35 -0500

    words of python

--- week_4.py
+++ week_4.py
@@ -1,4 +1,10 @@
 # Chapter Two: Variables and Expressions
 # 2.1 Expression
-# Reserved words
-words = ['false', 'class']
+# Reserved words: don't uses this words for name functions, don't name classes, don't name variables.
+words = ['false', 'class', 'return', 'is', 'finally',
+         'none', 'if', 'for', 'lambda', 'continue',
+         'true', 'def', 'from', 'while', 'nonlocal',
+         'and', 'del', 'global', 'not', 'with',
+         'as', 'elif', 'try', 'or', 'yield',
+         'assert', 'else', 'import', 'pass'
+         'break', 'except', 'in', 'raise']
