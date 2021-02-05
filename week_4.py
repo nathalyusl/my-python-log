105241dc65f65d164b4e8c2eeadae7db4f361361
nathalyusl <nathalyusl@gmail.com>
2021-02-05 16:37:50 -0500

    variables of python

--- week_4.py
+++ week_4.py
@@ -147,5 +147,8 @@ for line in handle:
 bigcount = None
 bigword = None
 for word, count in counts.items():
-    if bigcount is None
+    if bigcount is None or count > bigcount:
+        bigword = word
+        bigcount = count
+
 
