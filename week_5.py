0fe9dd2f6fa872bad4624459b9339ba3d02949b3
nathalyusl <nathalyusl@gmail.com>
2021-02-10 17:31:42 -0500

    decrease or maitain after if or for

--- week_5.py
+++ week_5.py
@@ -62,11 +62,11 @@ x = 5
 if x > 2:
     print('Bigger than 2')
     print('still bigger')
-print('Done with 2')
+print('Done with 2') # only sequential code
 
 for i in range(5):
     print(i)
     if i > 2:
-        print('BIgger than 2')
+        print('Bigger than 2')
     print('Done with i', i)
-print('All Done')
+print('All Done') # only sequential code
