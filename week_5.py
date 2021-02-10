8af2ea063bbad660220d6794f7e790f56af7dec1
nathalyusl <nathalyusl@gmail.com>
2021-02-10 18:22:59 -0500

    multi-way

--- week_5.py
+++ week_5.py
@@ -100,3 +100,16 @@ elif x < 10:
 else:
     print('LARGE')
 print('All done')
+
+if x < 2:
+    print('Small')
+elif x < 10:
+    print('Medium')
+elif x < 20:
+    print('Big')
+elif x < 40:
+    print('Large')
+elif x < 100:
+    print('Huge')
+else:
+    print('Ginormous')
