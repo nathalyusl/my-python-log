1ed083ca51a2a1905af11c0b1a7be4f3edd1906b
nathalyusl <nathalyusl@gmail.com>
2021-02-10 17:12:27 -0500

    hrs

--- week_5.py
+++ week_5.py
@@ -38,3 +38,19 @@ if x <= 5:
 
 if x != 6:
     print('Not equal 6')
+
+# One-way Decisions
+
+x = 5
+print('Before 5')
+if x == 5 :
+    print('Is 5')
+    print('Is Still 5')
+    print('Third 5')
+print('afterwards 5')
+print('Before 6')
+if x == 6 :
+    print('Is 6')
+    print('Is still 6')
+    print('Third 6')
+print('Afterwards 6')
