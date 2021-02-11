30d7cbe6eb5284c4a9ba4efa7a010a8150f68a2d
nathalyusl <nathalyusl@gmail.com>
2021-02-10 19:36:12 -0500

    try and except

--- week_5.py
+++ week_5.py
@@ -147,4 +147,23 @@ try:
 except:
     istr = -1
 
-print('Done', istr)
+print('Done', istr)astr = '123'
+try:
+    istr = int(astr)
+except:
+    istr = -1
+
+print('Second', istr)
+
+# Sample try/except
+
+rawstr = input('Enter a number:')
+try:
+    ival = int(rawstr)
+except:
+    ival = -1
+
+if ival > 0:
+    print('Nice work')
+else:
+    print('Not a number')
