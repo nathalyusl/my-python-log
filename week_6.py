e820b64eb52c720d4bf94fa1e200cc5788d92e62
dianaaltamirano <nathalyusl@gmail.com>
2021-11-18 12:57:15 -0500

    use the input

--- week_6.py
+++ week_6.py
@@ -2,6 +2,11 @@ def n1,n2 int
 print "Hola buenos dias"
 print("programa que realiza la suma de dos numeros")
 
+print('Ingrese un numero para realizar la suma')
+n1 = input()
+
+
+
 def dos ():
     x=1
     s=0
@@ -11,3 +16,4 @@ def dos ():
             s += x+y
             y += 1
         x += 1
+    print(s)
