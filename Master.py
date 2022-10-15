5e6ae710281b1fc5ce3059fb31149e33021d1104
dianaaltamirano <nathalyusl@gmail.com>
2022-10-15 11:03:18 -0400

    use the input

---
+++ Master.py
@@ -0,0 +1,9 @@
+a=2
+b=3
+c=4
+d=12
+e=5
+
+resul= 2*3+4-12**5
+
+print('El resultado de 2 * 3 +-12**5 es', resul)
--- week_7.js
+++ week_7.js
@@ -5,3 +5,7 @@ numero_1 = 10;
 numero_2 = 5;
 resultado = numero_1 + numero_2
 alert(resultado)
+
+numero += 5
+
+document.write(numero)
