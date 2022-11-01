2ec8828b20bb4c36900299389470e3f4f88063ae
dianaaltamirano <nathalyusl@gmail.com>
2022-11-01 08:27:29 -0400

    isdigit use

--- week_9.py
+++ week_9.py
@@ -4,5 +4,18 @@ print('Used the first time visual studio code')
 a = 10
 print(type(a))
 
-a = 'hola'   #Asignacion dinamica
+a = 'hola'   #Asignacion dinamica solo en Phyton
 print(type(a))
+
+# Ejercicio
+
+print('Ingresar un número: ')
+b = int(input())
+
+if type(b) == int:
+    print('a es un entero')
+
+c = input('Ingrese un número: ')
+
+if type(c) == str:
+    print('Es una cadena de texto')
