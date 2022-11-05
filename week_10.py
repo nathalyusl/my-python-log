148e2ba953e740296307b29b032395ce0d4ac67f
dianaaltamirano <nathalyusl@gmail.com>
2022-11-05 10:44:26 -0400

    random

--- week_10.py
+++ week_10.py
@@ -9,3 +9,32 @@ math.pow(2,3)
 # Factorial 
 
 math.factorial(5)
+
+#Uso 
+import math
+
+num = int(input('Ingrese un número: '))
+expo = int(input('Ingrese el número de iteraciones: '))
+
+acum = 0 
+for i in range(0,expo+1):
+  acum = acum + (math.pow(num,i)/ math.factorial(i))
+
+print(acum)
+
+# La lista son muy flexibles y puede ser una desventaja 
+lista = [1,2,3]
+print(lista)
+
+lista[2] = 'Miguel'
+print(lista)
+
+#Agregar un dato a la lista 
+lista.append('Hola')
+print(lista)
+
+import numpy as np
+
+a = np.random.random([10, 4])
+
+print(a)
