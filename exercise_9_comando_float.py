0ea0fb7a73ad828d0bbd426303bf9478c8534f45
dianaaltamirano <nathalyusl@gmail.com>
2022-11-15 12:12:01 -0500

    comando int exercise

--- exercise_9_comando_float.py
+++ exercise_9_comando_float.py
@@ -3,3 +3,12 @@ x = input()
 y = float(x) + 2
 
 print(ss + str(y))    # "+" concatena cadena o las suma solo se concatena str
+
+# <-------- Exercises ----------->
+print("Ingrese un número decimal: ")
+x = input()
+y = float(x) + 2.2
+aa = "El número decimal sumado 2.2 es: "
+
+print(aa + str(y))   #si "y" no se cambia a str lo que pasara es que al concatenar dará un error.
+
