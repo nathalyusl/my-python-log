5d2f44a774d385eaa36625254e416f3bbdcc1ef5
dianaaltamirano <nathalyusl@gmail.com>
2022-11-15 11:46:27 -0500

    Operaciones simples aritmeticas

---
+++ exercise_6_comando_str.py
@@ -0,0 +1,14 @@
+x = 9
+y = 7
+z = x-y
+stri_1 = "El resultado de la operación es: "
+
+print(stri_1+str(z)) #str transforma a una cadena
+
+# <-------- Exercises ----------->
+
+a = 4
+b = 6
+c = a + b
+pp = "La suma es:"
+print(pp+)
