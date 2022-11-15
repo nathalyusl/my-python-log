ffa1788e862f4a7d507231f90913bce8eabbd52e
dianaaltamirano <nathalyusl@gmail.com>
2022-11-15 12:02:17 -0500

    comando int exercise

--- exercise_8_comando_int.py
+++ exercise_8_comando_int.py
@@ -3,3 +3,12 @@ z = input()
 x = int(z) + 2
 
 print(pp+str(x))
+
+# <-------- Exercises ----------->
+
+print("Ingrese un número: ")
+z = input()
+a = int(z) * 2 #Le transforma a entero para hacer multiplicacion
+pp = "El número multiplicado por dos es: "
+
+print(pp + str(a))
