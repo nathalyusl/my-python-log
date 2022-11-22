b6751414793e866ea04bbe5fe7131d0aaadf80fe
dianaaltamirano <nathalyusl@gmail.com>
2022-11-22 00:54:00 -0500

    primeros comandos

--- week_14_pandas.py
+++ week_14_pandas.py
@@ -3,3 +3,16 @@ import pandas as pd
 df = pd.read_csv('Data_frame/simple_data.csv')
 
 print(df)
+
+dt = pd.read_csv('Data_frame/african_crises.csv')
+
+#Estadisticas
+print(df.describe())
+print('___')
+
+#Los primeros diez datos
+print(df.head(10))
+print('___')
+
+#Los ultimos 5 registros
+print(df.tail(5))
