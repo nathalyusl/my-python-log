322bc45abfb3a3c809d803ce913a073894ace328
dianaaltamirano <nathalyusl@gmail.com>
2022-11-22 00:27:00 -0500

    Agregar data frame imprimir data frame

---
+++ week_15_control_2.py
@@ -0,0 +1,8 @@
+# <-------------- CONTROL_2 ------------>
+# Resuelva el problema expuesto en un único script en Python.
+# Nombres: Diana Nathaly Altamirano Diaz / Nicolas Rebolledo
+print('')
+print('Programa_2')
+print('')
+print('En este acertijo los niños deben responder cuántos días tarda una rana en salir de un pozo de cierta profundidad, dado que de día avanza una determinada cantidad de metros y por la noche retrocede una cierta cantidad de metros.')
+print('')
