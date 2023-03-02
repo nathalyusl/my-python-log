d8e381cb7a57754150d0ad248cb7077f8cfed645
dianaaltamirano <nathalyusl@gmail.com>
2023-03-01 20:46:33 -0500

    concatenar

--- 01_introducion.py
+++ 01_introducion.py
@@ -1,6 +1,17 @@
+# Exercise_1
+# Se puede usar comillas simples o compuesta
+
 print('Hi how you doing')
 print('Hola como estas')
 
+# Exercise_2
 
+print('Hola Diana')
+#Error: Syntax error
 
+#Exercise_3
+
+print('Hola','Diana Nathaly')
+
+print('Hola'+'Diana Nathaly')  #concatena sin espacio
 
