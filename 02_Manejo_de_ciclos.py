4d94c9cef640dd0e5d90dd12dd20ffcdfadae7c1
dianaaltamirano <nathalyusl@gmail.com>
2023-08-22 19:53:48 -0400

    Manejo de ciclos

---
+++ 02_Manejo_de_ciclos.py
@@ -0,0 +1,9 @@
+#Programa
+
+resp = input('Le gustan los dulces?')
+
+if (resp == 'Sí' or resp == 'Si' or resp == 'si' or resp == 'sí'):
+    print('A mi tambien')
+
+
+#__________ Other way ________#
