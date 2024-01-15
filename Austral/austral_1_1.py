5bb110f7d85312f5e4cc7b6492b6228695aa90a3
dianaaltamirano <nathalyusl@gmail.com>
2024-01-15 08:32:27 -0500

    Uso del if

--- Austral/austral_1_1.py
+++ Austral/austral_1_1.py
@@ -9,3 +9,15 @@ math.sqrt(4)
 el_mundo_redondo = True
 if el_mundo_redondo:
     print("La tiera es redonda")
+
+
+x=1
+z=3
+x= (x+z)*2
+y= x/3
+m= x- y
+z = y
+
+text = 'Hello World'
+r = text[6]
+print(r)
