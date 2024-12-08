d14fbf984c5692f78b1c05295d73143a320053e5
dianaaltamirano <nathalyusl@gmail.com>
2024-12-08 17:57:25 -0500

    Use if

--- Santander_course/hello_world.py
+++ Santander_course/hello_world.py
@@ -3,3 +3,17 @@ print("Hello, World!")
 """
 Conditional Structures 
 """
+# IF
+age = 18
+
+if age >= 18:
+    print("You are an adult.")
+
+#IF - ESLSE
+
+age = 15
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
