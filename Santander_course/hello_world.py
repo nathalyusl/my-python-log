688d162aeaa2a420687b95ba465d1e0ad3d8dd4b
dianaaltamirano <nathalyusl@gmail.com>
2024-12-08 18:00:47 -0500

    Use if

--- Santander_course/hello_world.py
+++ Santander_course/hello_world.py
@@ -17,3 +17,12 @@ if age >= 18:
     print("You are an adult.")
 else:
     print("You are a minor.")
+
+# IF-ELIF-ELSE
+
+grade = input(int("Grade: "))
+
+if grade >=90:
+    print("Excellent")
+elif grade >= 80:
+    print("Very good")
