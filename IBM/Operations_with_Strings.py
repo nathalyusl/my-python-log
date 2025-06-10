7903d9125a4f08ee0ed7390081ec7e463a194dee
Nathaly <nathalyusl@gmail.com>
2025-06-09 17:35:35 -0700

    Uso the escapes \t, \n, \\, r

--- IBM/Operations_with_Strings.py
+++ IBM/Operations_with_Strings.py
@@ -23,5 +23,84 @@ name[1:9:3] # Resultado 'ia '
 len(name)  # 15 caracteres
 
 #____________Concatenar o combinar cadena________________
+
 name = "Michael Jackson"
-statement= name + "is the best"
+statement= name + " is the best"
+print(statement)   #  resultado = Michael Jackson is the best
+
+#___________ Operacion de multiplicacion_____________
+
+operacion = 3*name  # resultado = 'Michael JacksonMichael JacksonMichael Jackson'
+print(operacion)
+
+#___________Strings:escapes sequences_______________
+
+# Hacer una nueva linea \n
+
+print("Michael Jackson \nis the best")  #Michael Jackson
+                                        #is the best
+
+# Tabulacion \t
+
+print("Michael Jackson \tis the best")  #Michael Jackson 	is the best
+
+# Poder escribir una barra \
+
+print("Michael Jackson \\is the best") # Michael Jackson \is the best
+
+# Variacion con r
+
+print(r"Michael Jackson \is the best")
+
+
+#______________USO DE UPPER OR LOWER ____________
+
+a = "Thriller is the sixth studio album"
+b = a.upper()
+print(b)  #Resultado: THRILLER IS THE SIXTH STUDIO ALBUM
+
+b= a.lower()
+print(b)   # Resultado = thriller is the sixth studio album
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
